109951fb04dfcb458832498e57396ced1193e911
vsrikrishna <vsrikrsna@gmail.com>
2015-01-10 20:09:26 -0600

    01/10/2015

--- Concurrency/multipleOutThreads.cpp
+++ Concurrency/multipleOutThreads.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <thread>
+#include <vector>
 using namespace std;
 
 const int MAX_NO_OF_THREADS = 10;
@@ -8,18 +9,21 @@ void any_random_func(int i){
 	cout << "Output from thread: " << i << endl;
 }
 int main(){
-	std::thread t[MAX_NO_OF_THREADS];
+	vector<thread> th;
+	
    //Launch a thread
-	for (int i = 0; i < 10; i++){
-        t[i] = std::thread (any_random_func, i);
+	for (int i = 0; i < MAX_NO_OF_THREADS; i++){
+		th.push_back(std::thread(any_random_func, i));
 	}
    
    //Join the thread with the main thread
-	for (int i = 0; i < MAX_NO_OF_THREADS; i++){
-		t[i].join();
+	for (auto &t:th){
+		t.join();
 	}
+
 	cout << "Output from main thread" << endl;
-   //Get input from console before closing it
-   getchar();
-   return 0;
+    
+	//Get input from console before closing it
+    getchar();
+    return 0;
 }

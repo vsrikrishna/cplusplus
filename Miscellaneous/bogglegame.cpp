4bc5d87d743208529f2440a403be934cb503acea
System Administrator <root@Sris-MacBook-Air.local>
2016-11-11 11:36:19 -0600

    11/11/2016

--- Miscellaneous/bogglegame.cpp
+++ Miscellaneous/bogglegame.cpp
@@ -1,5 +1,5 @@
-
-ggle game - given a board of letters (2d array) and a word (string), 
+/*
+* boggle game - given a board of letters (2d array) and a word (string), 
 * return whether the word exists in the board. From each letter you can move 
 * in all directions (including diagonals), but you cannot use the same letter twice.  
 */
